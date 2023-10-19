d2d145512881f51cab759f501845b8b4bbed4644
NicolayJ <dnjimenez6@utpl.edu.ec>
2023-10-19 17:11:44 -0500

    Este es mi primer programa que subo de la clase

---
+++ dnjimenez6-MiPrimerPrograma/src/main/java/com/mycompany/dnjimenez6/miprimerprograma/Dnjimenez6MiPrimerPrograma.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.dnjimenez6.miprimerprograma;
+
+/**
+ *
+ * @author SALAI
+ */
+public class Dnjimenez6MiPrimerPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundito!");
+    }
+}
