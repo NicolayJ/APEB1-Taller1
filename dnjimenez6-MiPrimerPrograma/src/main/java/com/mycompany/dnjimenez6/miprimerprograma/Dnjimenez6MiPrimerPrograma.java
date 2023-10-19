b8bcbe888bb91496f243d764a04a13965c819c93
NicolayJ <dnjimenez6@utpl.edu.ec>
2023-10-19 17:33:40 -0500

    Hola mundo profe

--- dnjimenez6-MiPrimerPrograma/src/main/java/com/mycompany/dnjimenez6/miprimerprograma/Dnjimenez6MiPrimerPrograma.java
+++ dnjimenez6-MiPrimerPrograma/src/main/java/com/mycompany/dnjimenez6/miprimerprograma/Dnjimenez6MiPrimerPrograma.java
@@ -9,6 +9,6 @@ package com.mycompany.dnjimenez6.miprimerprograma;
 public class Dnjimenez6MiPrimerPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundito!");
+        System.out.println("Hola mundo profe");
     }
 }
